b8ae25bb518608cc47ef494875d3a18ab9c19c78
fogflea <fogflea@qq.com>
2023-09-10 14:21:58 +0800

    添加非post cover图片

---
+++ js/not-post-cover.js
@@ -0,0 +1,18 @@
+
+function changeCover(title){
+    imgs={"标签":"https://images.unsplash.com/photo-1571907483086-3c0ea40cc16d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+          "分类":"https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+          "Archives":"https://images.unsplash.com/photo-1530286443292-077db8d466a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1528&q=80",
+          "关于我":"https://s1.imagehub.cc/images/2023/08/25/fogflea.gif"}
+    document.getElementById("page-header").style.backgroundImage="url("+imgs[title]+")";
+}
+function findTitle(){
+    Ids=["site-title","nav-title"];
+    for(var i=0;i<Ids.length;i++){
+        if(document.getElementById(Ids[i])==null){
+            continue;
+        }
+        return document.getElementById(Ids[i]).innerHTML;
+    }
+}
+changeCover(findTitle());
