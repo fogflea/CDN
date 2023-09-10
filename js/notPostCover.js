ef1d5941aa30acf6f1cfe35070f78a7d971d1e15
fogflea <fogflea@qq.com>
2023-09-10 14:48:23 +0800

    优化

--- js/notPostCover.js
+++ js/notPostCover.js
@@ -5,6 +5,9 @@ function changeCover(title){
           "Archives":"https://images.unsplash.com/photo-1530286443292-077db8d466a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1528&q=80",
           "关于我":"https://s1.imagehub.cc/images/2023/08/25/fogflea.gif"}
     if(title!="标签"&&title!="分类"&&title!="Archives"&&title!="关于我"){
+        if(title!="Fogflea's blog"){
+            document.getElementById("page-header").style.backgroundImage+=",linear-gradient(rgba(34, 34, 34, 0.8),rgba(34, 34, 34, 0.3))"
+        }
         return;
     }
     document.getElementById("page-header").style.backgroundImage="linear-gradient(rgba(34, 34, 34, 0.8),rgba(34, 34, 34, 0.3)),url("+imgs[title]+")";
